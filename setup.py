49ad25f0ccc231164a63eed28c57e0862e8d20ff
Сергей Власов <sergei.vlasov@playrix.com>
2019-06-10 14:18:57 +0700

    Include Manifest

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import find_packages, setup
 
+
+# Package meta-data.
+NAME = 'dvalidator'
+DESCRIPTION = 'dvalidator validation attributes'
+LICENSE = 'MIT'
+AUTHOR = 'Vlasov Sergei'
+EMAIL = 'kakabara@outlook.com'
+URL = 'https://github.com/kakabara/dvalidator'
+VERSION = '0.1.2'
+
 setup(
-    name='dvalidator',
-    version='0.15',
-    description='dvalidator validation attributes',
-    license='MIT',
-    author='Vlasov Sergei',
-    author_email='kakabara@outlook.com',
-    url='https://github.com/kakabara/dvalidator',
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
     packages=find_packages(),
     install_requires=[
         'ipaddress==1.0.22',
